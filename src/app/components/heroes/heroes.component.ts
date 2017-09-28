d08449b30d0c1aed38764a1799befb5610b0c3f3
dktomaszenko <dktomaszenko@gmail.com>
2017-09-28 12:13:33 +0200

    creando la tabla con los heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -11,12 +11,14 @@ export class HeroesComponent implements OnInit {
 
   public heroes: Heroe[];
 
-  constructor(private heroesService: HeroesService) {
+  constructor(
+    private heroesService: HeroesService
+  ) {
 
     this.heroesService.getHeroes()
       .subscribe(heroes => {
         this.heroes = heroes;
-        console.log(this.heroes);
+
       });
 
   }
--- src/app/pipes/keys.pipe.ts
+++ src/app/pipes/keys.pipe.ts
@@ -1,12 +1,16 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 
 @Pipe({
   name: 'keys'
 })
 export class KeysPipe implements PipeTransform {
 
-  transform(value: any, args?: any): any {
-    return null;
+  transform(value: any): any {
+    let keys = [];
+    for (let key in value) {
+      keys.push(key);
+    }
+    return keys;
   }
 
 }
